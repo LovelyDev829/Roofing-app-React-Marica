664a5e3d03e43aa12e93295779d242c4e3ec5693
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-09-16 02:23:47 +0200

    left bottom cornor

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
 import './App.scss';
 import GridLines from "react-gridlines";
 import { useState } from 'react';
-const maxHeight = 20;
-const maxWidth = 60;
+const cellWidth = 44, cellHeight = 14;
+const maxHeightNum = 30;
+const maxWidthNum = 20;
+// var fieldFlag = []
 
 function App() {
-  const [height, setHeight] = useState(20)
-  const [bottomWidth, setBottomWidth] = useState(60)
-  const [topWidth, setTopWidth] = useState(36)
+
+  const [height, setHeight] = useState(25)
+  const [bottomWidth, setBottomWidth] = useState(16)
+  const [topWidth, setTopWidth] = useState(10)
+
   return (
     <div className="App">
       <div className='header'></div>
-      <div className='main'>
+      <div className='main' id='number'>
         <div className='left'>
           <div className='input-item'>
             <p>HELGHT</p>
-            <input type="text" onChange={(e)=>{setHeight(e.target.value)}} value={height}/>
+            <div className='flex'>
+              <input type="text" onChange={(e) => { setHeight(e.target.value) }} value={height} />
+              <p>X   35 =</p>
+              <input type="text" onChange={(e) => { setHeight(e.target.value/35) }} value={height*35} />
+              <p>(M)</p>
+            </div>
           </div>
           <div className='input-item'>
             <p>TOP-WIDTH</p>
-            <input type="text" onChange={(e)=>{setTopWidth(e.target.value)}} value={topWidth}/>
+            <div className='flex'>
+              <input type="text" onChange={(e) => { setTopWidth(e.target.value) }} value={topWidth} />
+              <p>X 110 =</p>
+              <input type="text" onChange={(e) => { setTopWidth(e.target.value/110) }} value={topWidth*110} />
+              <p>(M)</p>
+            </div>
           </div>
           <div className='input-item'>
             <p>BOTTOM-WIDTH</p>
-            <input type="text" onChange={(e)=>{setBottomWidth(e.target.value)}} value={bottomWidth}/>
+            <div className='flex'>
+              <input type="text" onChange={(e) => { setBottomWidth(e.target.value) }} value={bottomWidth} />
+              <p>X 110 =</p>
+              <input type="text" onChange={(e) => { setBottomWidth(e.target.value/110) }} value={bottomWidth*110} />
+              <p>(M)</p>
+            </div>
           </div>
           <div className='button'>Roof</div>
         </div>
         <div className='right'>
-          <div className='draw-area'>
+          <div className='draw-area' style={{
+            width: `${cellWidth * maxWidthNum + 1}px`, height: `${cellHeight * maxHeightNum}px`,
+            padding: `${cellHeight}px ${cellWidth}px ${cellHeight}px ${cellWidth}px`
+          }}>
             <GridLines
               className="grid-background"
-              cellHeight={20}
-              cellWidth={80}
+              cellHeight={cellHeight}
+              cellWidth={cellWidth}
               strokeWidth={1}
             ></GridLines>
-            <div className='polygon' style={{
-              clipPath: `polygon(
-            ${50 - topWidth / maxWidth * 50}% ${50 - height / maxHeight * 50}%,
-            ${50 + topWidth / maxWidth * 50}% ${50 - height / maxHeight * 50}%,
-            ${50 + bottomWidth / maxWidth * 50}% ${50 + height / maxHeight * 50}%,
-            ${50 - bottomWidth / maxWidth * 50}% ${50 + height / maxHeight * 50}%
-          )`}}></div>
+            <div className='polygon' style={{ clipPath: `polygon(
+              ${50-topWidth/maxWidthNum*50    - Math.min(50-topWidth/maxWidthNum*50, 50-bottomWidth/maxWidthNum*50)}% ${100-height/maxHeightNum*100}%,
+              ${50+topWidth/maxWidthNum*50    - Math.min(50-topWidth/maxWidthNum*50, 50-bottomWidth/maxWidthNum*50)}% ${100-height/maxHeightNum*100}%,
+              ${50+bottomWidth/maxWidthNum*50 - Math.min(50-topWidth/maxWidthNum*50, 50-bottomWidth/maxWidthNum*50)}% 100%,
+              ${50-bottomWidth/maxWidthNum*50 - Math.min(50-topWidth/maxWidthNum*50, 50-bottomWidth/maxWidthNum*50)}% 100%
+            )`}}></div>
           </div>
         </div>
       </div>
