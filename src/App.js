31bf86821aaf32f50cdd46672d9631c1b3312562
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-09-20 12:35:12 +0200

    useState update

--- src/App.js
+++ src/App.js
@@ -187,6 +187,9 @@ function App() {
     setColumnBottomPoint([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     setColumnHeight([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
   }
+  useEffect(() => {
+    console.log("polygon", polygon)
+  }, [polygon])
   return (
     <div className="App">
       <div className={elementName === '' ? 'hidden' : 'element-name'} style={{
@@ -200,13 +203,13 @@ function App() {
         <p>L-Opt Problem</p>
       </div>
       <div className='main' id='number'>
-        <div className={leftBarFlag ? 'slide-right left' : 'left'} onClick={() => { setRightBarFlag(false);}} >
+        <div className={leftBarFlag ? 'slide-right left' : 'left'} onClick={() => { setRightBarFlag(false); }} >
           <div className='left-bar-button' onClick={() => { setLeftBarFlag(!leftBarFlag) }}>
             <RightWard style={leftBarFlag ? { transform: 'rotate(180deg)' } : {}} />
           </div>
           <div className='button' onClick={() => { setDrawMode(!drawMode); initialize() }}>{drawMode ? 'INPUT MODE' : 'DRAW MODE'}</div>
           <div className={drawMode ? 'hidden' : 'input-mode'}>
-            <div className='input-item'>initialize
+            <div className='input-item'>
               <p>HELGHT</p>
               <div className='flex'>
                 <input type="number" step={0.1} min={0} max={30} value={Number(height).toFixed(1)}
@@ -254,27 +257,27 @@ function App() {
           <div className={drawMode ? 'input-mode' : 'hidden'}>
             {
               polygon?.map((item, index) => {
-                // let tempPolygon = polygon
                 return (
                   <div className='input-item' key={'draw-input-item' + index}>
                     <p>Point {index + 1}</p>
                     <div className='flex'>
                       <p>X : </p>
-                      <input type="number" step={0.1} min={0} max={22} value={Number(item.x * unitWidth * maxWidthNum / 100).toFixed(1)}
+                      <input type="number" step={0.1} min={-22} max={22} value={Number(item.x * unitWidth * maxWidthNum / 100).toFixed(1)}
                         onChange={(e) => {
-                          // console.log("from", tempPolygon)
-                          // console.log("step", e.target.value*100/(unitWidth*maxWidthNum))
-                          // tempPolygon[index].x = e.target.value*100/(unitWidth*maxWidthNum)
-                          // console.log("to",tempPolygon)
-                          // setPolygon(tempPolygon)
+                          let tempPolygon = polygon
+                          tempPolygon[index].x = e.target.value*100/(unitWidth*maxWidthNum)
+                          setPolygon([...tempPolygon])
+                          setDisplayFlag(false)
+                          setBottomLeft()
                         }} />
                       <p>Y : </p>
-                      <input type="number" step={0.1} min={0} max={10.5} value={Number((100-item.y) * unitHeight * maxHeightNum / 100).toFixed(1)}
+                      <input type="number" step={0.1} min={-10.5} max={10.5} value={Number((100 - item.y) * unitHeight * maxHeightNum / 100).toFixed(1)}
                         onChange={(e) => {
-                          // console.log("from", tempPolygon)
-                          // tempPolygon[index].y = 100-(e.target.value*100/(unitHeight*maxHeightNum))
-                          // console.log("to", tempPolygon)
-                          // setPolygon(tempPolygon)
+                          let tempPolygon = polygon
+                          tempPolygon[index].y = 100-(e.target.value*100/(unitHeight*maxHeightNum))
+                          setPolygon([...tempPolygon])
+                          setDisplayFlag(false)
+                          setBottomLeft()
                         }} />
                       <p>(M)</p>
                     </div>
@@ -288,12 +291,12 @@ function App() {
             <div className='button' onClick={() => { displayCalc(); setDisplayFlag(true) }}>ROOF</div>
           </div>
         </div>
-        <div className='middle' onClick={() => { if (rightBarFlag) {setRightBarFlag(false); setLeftBarFlag(true)} }}>
+        <div className='middle' onClick={() => { if (rightBarFlag) { setRightBarFlag(false); setLeftBarFlag(true) } }}>
           <div className={rightBarFlag ? 'draw-area move-left' : 'draw-area'} style={{
             width: `${cellWidth * maxWidthNum}px`, height: `${cellHeight * maxHeightNum}px`,
           }}
             onClick={(event) => {
-              if(!drawMode || displayFlag || polygon.length >= 8) return
+              if (!drawMode || displayFlag || polygon.length >= 8) return
               let currentTargetRect = event.currentTarget.getBoundingClientRect();
               const x = (event.pageX - currentTargetRect.left) / 8.8
               const y = (event.pageY - currentTargetRect.top) / 4.2
